ade022d7580672032c72f958cc2ddee86e02c814
maddyb99 <maddyb99@users.noreply.github.com>
2019-07-01 21:15:49 +0530

    working add

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+//import * as admin from 'firebase-admin'
+const admin= require('firebase-admin');
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+ console.log("hey");
+    response.send("Hello from this is maddy!");
+});
+
+export const addUserData = functions.https.onCall((data,context)=>
+{
+    admin.initializeApp();
+    let docID=String;
+    docID=data["docID"];
+    const users = admin.firestore().doc('Users/'+docID);
+    console.log(docID);
+    //data.delete("docID");
+    return users.set(data);
+}
+)
